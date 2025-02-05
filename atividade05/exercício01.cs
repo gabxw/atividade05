0d1444d1073450e4d9a8ffd8c235b04deac85e44
Gabriel José Maciel <gabriel.jmaciel13@gmail.com>
2025-02-05 09:45:57 -0300

    Adicionar arquivos de projeto.

---
+++ atividade05/exercício01.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atividade05
+{
+    internal class exercício01
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escreva um numero");
+            var num = int.Parse(Console.ReadLine());
+
+
+            var resultado = num % 2 == 0 ? "O número é par" : "O numero é impar";
+
+            Console.WriteLine(resultado);
+
+        }
+    }
+}
